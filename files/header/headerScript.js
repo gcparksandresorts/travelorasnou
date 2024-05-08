5666d82100f925955362a3c1209dcb2f5c4fde53
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-05-08 14:21:02 -0700

    Create headerScript.js

---
+++ files/header/headerScript.js
@@ -0,0 +1,31 @@
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+const loader = document.getElementById("loader");
+
+function endPageLoad() {
+	// Close the loader after delay num
+	loader.style.opacity = 0;
+  	setTimeout(function(){loader.style.display='none'},1000);
+}
+
+// Create the Header and Populate it Depending on Screen Size
+function createHeader(){
+	console.clear();
+	header.innerHTML = "<div id='headerSpacer'></div><div id='headerBar'></div>";
+
+	const headerBar = document.getElementById("headerBar");
+
+	headerBar.innerHTML = "<img src='https://gcparksandresorts.github.io/main/webresources/images/GCPR-logo.png' id='headerLogo'>";
+
+	createFooter();
+}
+
+// If small screen width, this menu will create the normal header elements inside the expandable menu
+function openHeaderMenu(){
+
+}
+
+// Create the Footer and Populate it
+function createFooter(){
+	
+}
